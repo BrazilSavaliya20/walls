2be012eaf4b7fc94c5bfba2e4ec872c1dd581c95
BrazilSavaliya20 <152050599+BrazilSavaliya20@users.noreply.github.com>
2025-08-26 16:23:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -318,5 +318,9 @@ def secret_admin():
 
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
